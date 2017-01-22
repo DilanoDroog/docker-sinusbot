dedc53deb966f2dbc4a22716bc193f0a0e40d917
Droog <github@dilanodroog.nl>
2017-01-22 21:22:41 +0100

    Readding YoutubeDLPath before starting the bot, not sure why it keeps dissapearing...

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 echo "Setting user permissions again"
 chown "$USER":"$GROUP" -R "$TS3BOT_DIR/data"
 
-echo "Setting the SameInterval and YoutubeDLPath again"
-sed -i "s|SampleInterval = .*|SampleInterval = \"500\"|g" "$TS3BOT_DIR/config.ini"
+echo "Setting the YoutubeDLPath"
 sed -i "s|YoutubeDLPath = .*|YoutubeDLPath = \"$YTDL_BIN\"|g" "$TS3BOT_DIR/config.ini"
 
 echo "Adding execute access to sinusbot"
